40357be8928d7181f07a3d8cd03d379ef3c07f98
directman66 <sannikovdi@yandex.ru>
2018-08-23 00:31:30 +0500

    Update slack.class.php

--- modules/slack/slack.class.php
+++ modules/slack/slack.class.php
@@ -221,6 +221,68 @@ $text=$message;
  }
 
 }
+
+ function sendMessageToAll($message) {
+
+$url = SETTINGS_SLACK_APIURL;
+$text=$message;
+ define('SLACK_WEBHOOK', $url); // это не забудьте поменять на свое
+  $message = array('payload' => json_encode(array('text' => $text)));
+  $c = curl_init(SLACK_WEBHOOK);
+  curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($c, CURLOPT_POST, true);
+  curl_setopt($c, CURLOPT_POSTFIELDS, $message);
+  curl_exec($c);
+  curl_close($c);
+
+}
+
+ function sendImageToAll($path, $message) {
+
+$url = SETTINGS_SLACK_APIURL;
+$text=$message;
+ define('SLACK_WEBHOOK', $url); // это не забудьте поменять на свое
+//  $message = array('payload' => json_encode(array('text' => $text,   "image_url"=> $path)));
+
+$json='"attachments": [
+        {
+            "fallback": "Required plain-text summary of the attachment.",
+            "color": "#36a64f",
+            "pretext": "Optional text that appears above the attachment block",
+            "author_name": "Bobby Tables",
+            "author_link": "http://flickr.com/bobby/",
+            "author_icon": "http://flickr.com/icons/bobby.jpg",
+            "title": "Slack API Documentation",
+            "title_link": "https://api.slack.com/",
+            "text": "Optional text that appears within the attachment",
+            "fields": [
+                {
+                    "title": "Priority",
+                    "value": "High",
+                    "short": false
+                }
+            ],
+            "image_url": '.$path.',
+            "thumb_url": "http://example.com/path/to/thumb.png",
+            "footer": "Slack API",
+            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+            "ts": 123456789
+        }
+    ]  ';
+// $message = array('payload' => $json);
+ $message = $json;
+
+
+  $c = curl_init(SLACK_WEBHOOK);
+  curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($c, CURLOPT_POST, true);
+  curl_setopt($c, CURLOPT_POSTFIELDS, $message);
+  curl_exec($c);
+  curl_close($c);
+
+}
+
+
 /**
 * Install
 *
